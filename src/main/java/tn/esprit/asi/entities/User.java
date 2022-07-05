df2d6ed9cf2f3979c268d545771aad1a0a780de3
aymenjeljli <aymenjeljli9@gmail.com>
2022-07-05 14:23:32 +0100

    Add User Entity

--- src/main/java/tn/esprit/asi/entities/User.java
+++ src/main/java/tn/esprit/asi/entities/User.java
@@ -1,10 +1,19 @@
 package tn.esprit.asi.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
