dd08914dba4ee00469339107f991bc129dde88eb
Rhouma Ines <ines.rhouma@esprit.tn>
2022-07-05 18:32:08 +0200

    crudes Ratting

---
+++ src/main/java/tn/esprit/asi/services/RatingService.java
@@ -0,0 +1,8 @@
+package tn.esprit.asi.services;
+
+import tn.esprit.asi.entities.Ratting;
+
+public interface RatingService {
+	public Ratting addEventRating(Ratting r,Long userId, Long eventId);
+	public Ratting showEventRating(Long userId, Long eventId);
+}
